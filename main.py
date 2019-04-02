f58889c1256836260de40f8a71a6d46e6b4d255c
C0NSTABEL <48985236+C0NSTABEL@users.noreply.github.com>
2019-04-02 16:10:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import praw
 import requests
 import bs4
-from urllib.parse import quote_plus
 import re
 
 reddit = praw.Reddit(
-    client_id='CLIENT_ID',
-    client_secret='SECRET',
-    username='brandnewsentencebot',
-    password='PASSWORD',
-    user_agent='AGENT')
-    
-subreddit = reddit.subreddit("testingground4bots")
+		client_id='',
+
+		client_secret='',
+
+		username='brandnewsentencebot',
+
+		password='',
+
+		user_agent='brandnewsentencebot by u/C0NSTABEL') #what even is this?
+
+subreddit = reddit.subreddit("all")
 keyphrase = "r/brandnewsentence"
-reply_template = "Actually, this phrase already exists in {}. ^((not counting special characters and numbers\\))\n\n\n[About the Library of Babel](https://libraryofbabel.info/About.html) \n\n^(I'm a bot, beep boop. Contact my creator u/C0NSTABEL to share your ideas, complaints or love for me :\\))"
-blacklist = ["sneakpeekbot", "allegedlybot"]
+reply_template = "Actually, this phrase already exists in {}. \n\n^((no special characters or numbers\\))\n\n\n^(I'm a bot, beep boop. To discuss me, contact my creator u/C0NSTABEL :\\))"
+blacklist = ["sneakpeekbot", "allegedlybot", "brandnewsentencebot"]
 URL = "https://libraryofbabel.info/search.cgi"
 
 def onclick_to_url(text):
@@ -40,30 +43,39 @@ def search(text):
     return str(exact_url)
 
 def main():
-    for comment in subreddit.stream.comments():
-        if keyphrase not in comment.body.lower():
-            continue
-        parent = comment.parent()
-        if type(parent) != praw.models.reddit.comment.Comment or str(comment.author.name).lower() in blacklist:
-            continue
-        alreadyReplied = False
-        comment.refresh()
-        comment.reply_limit = 15
-        for replies in comment.replies:
-            if comment.replies:
-                if str(replies.author).lower() == "brandnewsentencebot":
-                    alreadyReplied = True
-        if alreadyReplied:
-            continue
-        if len(parent.body) > 3199:
-        	continue
-        print("replying to: {}".format(comment.author))
-        print("reddit.com{}".format(comment.permalink))
-        query = parent.body
-        library_of_babel_url = search(query)
-        library_of_babel_rlink = f"[The Library Of Babel]({library_of_babel_url})"
-        full_reply = reply_template.format(library_of_babel_rlink)
-        comment.reply(full_reply)
+	fail_counter = 0
+	for comment in subreddit.stream.comments():
+		if keyphrase not in comment.body.lower() or comment.subreddit == "BrandNewSentence": #this should prevent it from showing up in that subreddit right?
+			continue
+		parent = comment.parent()
+		if type(parent) != praw.models.reddit.comment.Comment or str(comment.author.name).lower() in blacklist:
+			continue
+		alreadyReplied = False
+		comment.refresh()
+		comment.reply_limit = 15
+		for replies in comment.replies:
+			if comment.replies:
+				if str(replies.author).lower() == "brandnewsentencebot":
+					alreadyReplied = True
+		if alreadyReplied:
+			continue
+		if len(parent.body) > 3199:
+			continue
+		print("replying to: {}".format(comment.author))
+		print("reddit.com{}".format(comment.permalink))
+		query = parent.body
+		library_of_babel_url = search(query)
+		library_of_babel_rlink = f"[here in The Library Of Babel]({library_of_babel_url})"
+		full_reply = reply_template.format(library_of_babel_rlink)
+		try:
+			comment.reply(full_reply)
+			reddit.redditor("C0NSTABEL").message("results!","reddit.com{}".format(comment.permalink))
+		except praw.exceptions.APIException:	#If the comment limit is reached it just skips that comment
+			fail_counter += 1
+			print("failure ({} times)".format(str(fail_counter)))
+			if fail_counter == 50:
+				reddit.redditor("C0NSTABEL").message("Bot stopped","bot is no longer running")
+				break
 
 if __name__ == "__main__":
 	main()
